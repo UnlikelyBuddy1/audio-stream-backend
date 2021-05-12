a1a64894922bca813af3adbb6fbf5857c983d37d
UnlikelyBuddy <UnlikelyBuddy@users.noreply.github.com>
2021-05-12 16:20:30 +0200

    grhrhtg

--- src/stream/stream.controller.ts
+++ src/stream/stream.controller.ts
@@ -4,10 +4,9 @@ import { AuthGuard } from '@nestjs/passport';
 @Controller('stream')
 @UseGuards(AuthGuard())
 export class StreamController {
-    @Get('/:trackpath')
-    @Header('Cache-Control', 'max-age=31536000')
-    seeUploadedFile(@Param('trackpath') track, @Res() res) {
+  @Get('/:trackpath')
+  @Header('Cache-Control', 'max-age=31536000')
+  seeUploadedFile(@Param('trackpath') track, @Res() res) {
     return res.sendFile(track, { root: './files' });
   }
 }
-
