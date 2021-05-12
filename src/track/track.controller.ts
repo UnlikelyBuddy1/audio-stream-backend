a152fa3756f9d2ef15376a7904243e50593d1bda
UnlikelyBuddy <UnlikelyBuddy@users.noreply.github.com>
2021-05-12 16:57:19 +0200

    ptn

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Get, Post, Param, Delete, Patch, UsePipes, ValidationPipe, ParseIntPipe, Query, UseGuards, UseInterceptors, UploadedFile, InternalServerErrorException } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete, Patch, UsePipes, ValidationPipe, ParseIntPipe, Query, UseGuards, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { Track } from 'src/entities/track.entity';
 import { User } from 'src/entities/user.entity';
-import { editFileName, imageFileFilter } from 'src/track/file-upload.utils';
 import { createTrackDto } from './dto/create-track.dto';
 import { GetTracksFilterDto } from './dto/get-tracks-filter.dto';
+import { editFileName, imageFileFilter } from './file-upload.utils';
 import { TrackService } from './track.service';
 
 
@@ -23,24 +23,22 @@ export class TrackController {
     }
 
     @Post()
+    @UseGuards(AuthGuard())
     @UsePipes(ValidationPipe)
     @UseInterceptors(
-        FileInterceptor('track', {
-          storage: diskStorage({
-            destination: './files',
-            filename: editFileName,
-          }),
-          fileFilter: imageFileFilter,
+      FileInterceptor('track', {
+        storage: diskStorage({
+          destination: './files',
+          filename: editFileName,
         }),
-      )
-    createTrack(@Body() createTrackDto: createTrackDto, @GetUser() user: User, @UploadedFile() file: Express.Multer.File): Promise<Track>{
-        if(!createTrackDto.path && !file){
-            throw new InternalServerErrorException('Must specify a path or upload a file')
-        }
-        console.log("there's something happening here");
-
+        fileFilter: imageFileFilter,
+      }),
+    )
+    createTrack(@Body() createTrackDto: createTrackDto, @GetUser() user: User, @UploadedFile() file: Express.Multer.File,): Promise<Track>{
+        console.log("someone posted a song")
         return this.trackService.createTrack(createTrackDto, user, file.filename);
     }
+
 
     @Delete('/:id')
     deleteTrack(@Param('id', ParseIntPipe) id: number, @GetUser() user: User): Promise<void> {
@@ -51,6 +49,5 @@ export class TrackController {
     getTracks(@Query(ValidationPipe) filterDto: GetTracksFilterDto, @GetUser() user: User): Promise<Track[]>  {
         return this.trackService.getTracks(filterDto, user);
     }
-
-
+    
 }
