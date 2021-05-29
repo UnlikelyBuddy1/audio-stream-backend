4829ae31965dca87b381dee0af9b1212619af296
UnlikelyBuddy <UnlikelyBuddy@users.noreply.github.com>
2021-05-30 00:05:27 +0200

    not the best but cover is nullable because of bug

--- src/entities/album.entity.ts
+++ src/entities/album.entity.ts
@@ -11,7 +11,7 @@ export class Album extends BaseEntity {
     @Column()
     name: string;
 
-    @Column()
+    @Column({nullable: true})
     cover: string;
 
     @ManyToMany( () => Track, track => track.albums)
