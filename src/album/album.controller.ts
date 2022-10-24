f580e6d571df7e9a9c558cfb0220e3f8686a408f
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2022-10-24 17:14:42 +0200

    removed auth for covers

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -12,7 +12,6 @@ import { GetAlbumsFilterDto } from './dto/get-albums-filter.dto';
 import { modifyAlbumDto } from './dto/modify-album-dto';
 
 @Controller('album')
-@UseGuards(AuthGuard())
 export class AlbumController {
     constructor(private albumService: AlbumService){}
 
--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -9,7 +9,6 @@ import { GetGenresFilterDto } from './dto/get-genre-filter.dto';
 import { modifyGenreDto } from './dto/modify-genre-dto';
 
 @Controller('genre')
-@UseGuards(AuthGuard())
 export class GenreController {
     constructor(private genreService: GenreService){}
 
